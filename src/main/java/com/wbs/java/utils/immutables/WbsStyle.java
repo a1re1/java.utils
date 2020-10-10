54ceb68b04eb80909d714dade71aeaa2b97d4231
Tyler Whitehurst <twwhitehurst@gmail.com>
2020-10-10 16:48:54 -0400

    add immutables

---
+++ src/main/java/com/wbs/java/utils/immutables/WbsStyle.java
@@ -0,0 +1,21 @@
+package com.wb.game.engine.utils.immutables;
+
+import org.immutables.value.Value;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.PACKAGE, ElementType.TYPE})
+@Retention(RetentionPolicy.CLASS) // Make it class retention for incremental compilation
+@Value.Style(
+        get = {"is*", "get*"},
+        init = "set*",
+        typeAbstract = {"*IF"},
+        typeImmutable = "*",
+        builder = "builder",
+        build = "build",
+        visibility = Value.Style.ImplementationVisibility.PUBLIC,
+        defaults = @Value.Immutable(copy = false))
+public @interface WbsStyle {}
